0cfc25677341a0280042cc3a981e5b455d3e79b7
hansung <gnidoc327@gmail.com>
2022-11-10 06:13:15 +0900

    Add eslint, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,37 @@
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [time, setTime] = useState(new Date());
+	const [time, setTime] = useState(new Date());
 
-  const timer = setInterval(() => {
-    setTime(new Date());
-  }, 1000);
-  
-  useEffect(() => {  
-    return () => {
-      clearInterval(timer);
-    };
-  }, [timer]);
+	const timer = setInterval(() => {
+		setTime(new Date());
+	}, 1000);
 
-  
-  return (
-    <div style={{whiteSpace: 'pre-line'}}>
-      <h3>현재 시간 : {time.toLocaleTimeString()}</h3>
+	useEffect(() => {
+		return () => {
+			clearInterval(timer);
+		};
+	}, [timer]);
 
-      <h3>자기소개</h3>
-      <h3>이름 : 김한성</h3>
-      <h3>학력</h3>
-      <>
-        <li>장훈고등학교</li>
-        <li>중앙대학교</li>
-      </>
-      <h3>경력</h3>
-      <>
-        <li>SKT</li>
-      </>
-    </div>
-  );
+	const a = { '1': 1, '2': 2 };
+
+	return (
+		<div style={{ whiteSpace: 'pre-line' }}>
+			<h3>현재 시간 : {time.toLocaleTimeString()}</h3>
+
+			<h3>자기소개</h3>
+			<h3>이름 : 김한성</h3>
+			<h3>학력</h3>
+			<>
+				<li>장훈고등학교</li>
+				<li>중앙대학교</li>
+			</>
+			<h3>경력</h3>
+			<>
+				<li>SKT</li>
+			</>
+		</div>
+	);
 }
 
 export default App;
